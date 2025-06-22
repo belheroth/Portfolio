551c16d4e0c446a6bc4f35767bbcb2033cfa4684
belheroth <134479638+belheroth@users.noreply.github.com>
2025-06-22 14:07:53 +0800

    Add files via upload

---
+++ kim.js
@@ -0,0 +1,28 @@
+
+document.querySelectorAll('nav ul li a').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+        
+    const href = this.getAttribute('href');
+    
+    document.querySelector(href).scrollIntoView({
+       behavior: 'smooth'
+    });
+  });
+});
+   const cards = document.querySelectorAll('.info-grid div');
+
+cards.forEach(card => {
+  card.addEventListener('mouseenter', () => {
+    card.style.transition = 'transform 0.3s ease-out';
+    
+     card.style.transform = 'scale(1.1)';
+ 
+  });
+
+  card.addEventListener('mouseleave', () => {
+    
+    card.style.transition = 'transform 0.3s ease-out';
+    card.style.transform = 'scale(1)';
+  });
+});
